74e040e1751aa21e1737dfb198ddafdf0503fca9
Wuanitawanjiku <wanjikuwuanita@gmail.com>
2021-08-30 09:43:17 +0300

    navbar styling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 
 import './App.css';
+import {Link, BrowserRouter as Router, Route}from 'react-router-dom';
 import Products from './Products.js';
 import Fruits from './Fruits';
 import Vegetables from './Vegetables';
@@ -8,10 +9,19 @@ import Vegetables from './Vegetables';
 function App() {
   return (
     <div className="App">
-      <Products/>
-      <Fruits/>
-      <Vegetables/>
-      
+      <Router>
+        <nav>
+          <ul className="navbar">
+            <Link to="/" style={{textDecoration:"none"}}><li>Products</li></Link>
+            <Link to="/fruits" style={{textDecoration:"none"}}><li>Fruits</li></Link>
+            <Link to="/vegetables" style={{textDecoration:"none"}}><li>Vegetables</li></Link>
+
+          </ul>
+        </nav>
+        <Route path="/" exact component = {Products}/>
+        <Route path="/fruits" exact component = {Fruits}/>
+        <Route path="/vegetables" exact component = {Vegetables}/>   
+         </Router>
     </div>
   );
 }
